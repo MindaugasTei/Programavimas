96c2084ee132c4fa2d1dce48f80c63a692fbee8d
MindaugasTei <129012732+MindaugasTei@users.noreply.github.com>
2023-04-03 13:02:43 +0300

    Uzduotis

---
+++ Namu darbai 04-03/Balsavimas.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Iveskite savo amziu: ");
+int amzius = int.Parse(Console.ReadLine());
+
+if (amzius >= 18)
+{
+    Console.WriteLine("Galite balsuoti!");
+}
+else
+{
+    Console.WriteLine("Negalite balsuoti.");
+}
