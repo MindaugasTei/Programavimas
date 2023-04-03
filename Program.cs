11e10af35a61bbf5797575afabd69640395050d3
MindaugasTei <129012732+MindaugasTei@users.noreply.github.com>
2023-04-03 12:15:29 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Sestauzduotis;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string marke = "audi";
+        int modelis = 100;
+        int metai = 1980;
+        int rida = 20000;
+        Console.WriteLine($"{marke} {modelis} {metai} {rida}");
+
+    }
+}
+
