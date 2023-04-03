1b907062b43e9e3218ab5fdba29d76bd868664f5
MindaugasTei <129012732+MindaugasTei@users.noreply.github.com>
2023-04-03 12:29:53 +0300

    Uzduotis

---
+++ Namu darbai 04-03/Uzduotis07.cs
@@ -0,0 +1,15 @@
+﻿namespace septintauzduotis;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iveskite savo varda");
+        string vardas = Console.ReadLine();
+        Console.WriteLine("Jusu vardas," + vardas);
+        Console.WriteLine("Iveskite savo amziu");
+       string amzius = Console.ReadLine();
+        Console.WriteLine("Jusu amzius " + amzius);
+
+    }
+}
+
