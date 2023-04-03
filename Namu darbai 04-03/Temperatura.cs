b83cb20ba474402ce648201e0a9f71ed9050f371
MindaugasTei <129012732+MindaugasTei@users.noreply.github.com>
2023-04-03 16:57:08 +0300

    Uzduotis

---
+++ Namu darbai 04-03/Temperatura.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Iveskite temperatura:");
+
+        int temperatura;
+        if (int.TryParse(Console.ReadLine(), out temperatura))
+        {
+            if (temperatura <= 0)
+            {
+                Console.WriteLine("Ziauria salta!");
+            }
+            else if (temperatura < 10)
+            {
+                Console.WriteLine("Labai salta.");
+            }
+            else if (temperatura < 20)
+            {
+                Console.WriteLine("Salta.");
+            }
+            else if (temperatura < 30)
+            {
+                Console.WriteLine("Normali temperatura.");
+            }
+            else if (temperatura < 40)
+            {
+                Console.WriteLine("Karsta!");
+            }
+            else if (temperatura < 50)
+            {
+                Console.WriteLine("Ziauriai karsta!");
+
+
+            }
+
+
+
+        }
+
+    }
+
+    }
