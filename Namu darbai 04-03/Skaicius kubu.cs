d069bd2fb2b0384b89862f70c07fecaff31ad3ad
MindaugasTei <129012732+MindaugasTei@users.noreply.github.com>
2023-04-03 12:33:40 +0300

    Uzduotis

---
+++ Namu darbai 04-03/Skaicius kubu.cs
@@ -0,0 +1,13 @@
+﻿namespace simbolis;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Iveskite skaiciu: ");
+        double skaicius = double.Parse(Console.ReadLine());
+        double rezultatas = Math.Pow(skaicius, 3);
+        Console.WriteLine($"{skaicius} Pakelta kubu yra: {rezultatas}");
+
+    }
+
+}
