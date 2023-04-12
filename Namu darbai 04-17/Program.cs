437f68934aee1d5261efe71449f12969874db421
MindaugasTei <129012732+MindaugasTei@users.noreply.github.com>
2023-04-12 16:25:50 +0300

    Namu darbai

---
+++ Namu darbai 04-17/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dvyliktauzduotis;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iveskite 3 skaicius:");
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+
+        int maxNum = Math.Max(num1, Math.Max(num2, num3));
+
+        Console.WriteLine("Didziausias skaicius yra: " + maxNum);
+    }
+}
+
